1be35e3cec8e8b6236435747156a08c75edff00c
Antra Verma <antraverma99@gmail.com>
2023-12-19 13:31:25 +0530

    added mobile nav

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,13 +1,26 @@
 "use client";
 
 import { Category } from "@/types";
+import { Menu } from "@headlessui/react";
+import { MenuSquare } from "lucide-react";
 
 interface MobileNavProps {
   data: Category[];
 }
 
 const MobileNav: React.FC<MobileNavProps> = ({ data }) => {
-  return <div>mobile nav</div>;
+  return (
+    <Menu>
+      <Menu.Button>
+        <MenuSquare className="h-8 w-8" />
+      </Menu.Button>
+      <Menu.Items>
+        {data.map((item) => (
+          <Menu.Item>{({ active }) => <div>{item.name}</div>}</Menu.Item>
+        ))}
+      </Menu.Items>
+    </Menu>
+  );
 };
 
 export default MobileNav;
