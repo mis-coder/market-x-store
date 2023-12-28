060b05d9bb8e70130a603247bd3ce2c26b1ea2df
Antra Verma <antraverma99@gmail.com>
2023-12-28 11:55:50 +0530

    added animation to context menu

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -21,41 +21,42 @@ interface MobileNavProps {
 
 const MobileNav: React.FC<MobileNavProps> = ({ data }) => {
   const pathname = usePathname();
-
   const routes = data.map((route) => ({
     href: `/category/${route.id}`,
     label: route.name,
     active: pathname === `/category/${route.id}`,
   }));
+
   return (
-    <div className="sm-flex md:hidden lg:hidden">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Menu className="cursor-pointer hover:scale-110" size="30" />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-48 mr-5 mt-1 bg-white" forceMount>
-          <DropdownMenuLabel className="font-bold">
-            <p>Categories</p>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator className="bg-gray-200" />
-          <DropdownMenuGroup>
-            {routes.map((item) => (
-              <DropdownMenuItem key={item.href}>
-                <Link
-                  href={item.href}
-                  className={cn(
-                    "text-sm font-medium cursor-pointer transition-colors hover:text-black",
-                    item.active ? "text-black" : "text-neutral-500"
-                  )}
-                >
-                  {item.label}
-                </Link>
-              </DropdownMenuItem>
-            ))}
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Menu className="cursor-pointer hover:scale-110" size="30" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-48 mr-5 mt-1 bg-white animate-slide-down-and-fade"
+        forceMount
+      >
+        <DropdownMenuLabel className="font-bold">
+          <p>Categories</p>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator className="bg-gray-200" />
+        <DropdownMenuGroup>
+          {routes.map((item) => (
+            <DropdownMenuItem key={item.href}>
+              <Link
+                href={item.href}
+                className={cn(
+                  "text-sm font-medium cursor-pointer transition-colors hover:text-black",
+                  item.active ? "text-black" : "text-neutral-500"
+                )}
+              >
+                {item.label}
+              </Link>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        "slide-down-and-fade": "slide-down 1s linear",
+      },
+      keyframes: {
+        "slide-down": {
+          from: { opacity: "0", transform: "translateY(-6px)" },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
