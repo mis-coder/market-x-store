f52ddd8f899f996d581f7fa304aacfefec88d536
Antra Verma <antraverma99@gmail.com>
2023-12-28 18:43:25 +0530

    optimized billboard image

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -1,4 +1,5 @@
 import { Billboard } from "@/types";
+import Image from "next/image";
 
 interface BillboardProps {
   data: Billboard;
@@ -7,11 +8,15 @@ interface BillboardProps {
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
   return (
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-      <div
-        style={{ backgroundImage: `url(${data?.imageUrl})` }}
-        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
-      >
-        <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
+      <div className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
+        <Image
+          className="h-full w-full object-cover"
+          src={data?.imageUrl}
+          alt="billboard-image"
+          width={500}
+          height={500}
+        />
+        <div className="absolute top-0 right-0 h-full w-full flex flex-col justify-center items-center text-center gap-y-8 bg-black text-white opacity-70">
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
             {data?.label}
           </div>
